b76c6aef0098b3a29648cd55fdc88926d5a022cc
Yuntai Kyong <you@example.com>
2017-09-28 09:22:42 +0000

    modify .gitignore

---
+++ dapp/README.md
@@ -0,0 +1,27 @@
+* Install MetaMask
+
+* Install truffle and an ethereum client
+  ```javascript
+  npm install -g truffle
+  npm install -g ethereumjs-testrpc
+  ```
+
+* Install node dependency.
+  ```javascript
+  npm install`
+  ```
+* Run testrpc
+  ```sh
+  testrpc
+  ```
+
+* Compile and migrate the contracts.
+  ```javascript 
+  truffle compile
+  truffle migrate
+  ```
+
+* Run the webpack server.
+  ```javascript
+  npm run start
+  ```
---
+++ dapp/src/contracts/contractSetup.js
@@ -0,0 +1,41 @@
+import { web3 } from '../util/connector.js'
+
+function MyContractSetup () {
+  let MyContractABI = web3.eth.contract([
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "share",
+          "type": "uint256"
+        }
+      ],
+      "name": "updateShares",
+      "outputs": [],
+      "payable": false,
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "addr",
+          "type": "address"
+        }
+      ],
+      "name": "getShares",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "type": "function"
+    }
+  ])
+  let MyContractObj = MyContractABI.at("0x71845bbfe5ddfdb919e780febfff5eda62a30fdc")
+  return MyContractObj
+}
+
+export { MyContractSetup }
