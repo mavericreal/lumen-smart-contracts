6e7c838fe4a2c77b34b528c491d4ecf7aef2d5fd
Yuntai Kyong <you@example.com>
2017-09-28 04:35:55 +0000

    added some changes related to uport

--- dapp/src/user/layouts/profile/Profile.js
+++ dapp/src/user/layouts/profile/Profile.js
@@ -11,11 +11,15 @@ class Profile extends Component {
       <main className="container">
         <div className="pure-g">
           <div className="pure-u-1-1">
-            <h1>Profile</h1>
+            <h1>My Profile</h1>
             <p>Change these details in UPort to see them reflected here.</p>
             <p>
               <strong>Name</strong><br />
               {this.props.authData.name}
+            </p>
+            <p>
+              <strong>Email</strong><br />
+              {this.props.authData.email}
             </p>
           </div>
         </div>
